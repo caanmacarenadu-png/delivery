86cb522e8973444cb8edbec4f860b0263febdcab
AldahirH <jealhernandezme@ittepic.edu.mx>
2026-09-09 23:45:05 -0700

    Configura API NestJS con MongoDB y modulo de usuarios

---
+++ apps/api/src/users/users.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersController } from './users.controller.js';
+import { UsersService } from './users.service.js';
+import { User, UserSchema } from './user.schema.js';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
